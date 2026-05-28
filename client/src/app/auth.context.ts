a2d04d5a566a488c621ae3467376cbae17fe5611
Vlad8800 <93032225+Vlad8800@users.noreply.github.com>
2026-05-28 17:14:26 +0300

    Refactor User role definitions in auth.context.ts
    Removed duplicate role definitions for User interface.

--- client/src/app/auth.context.ts
+++ client/src/app/auth.context.ts
@@ -3,11 +3,8 @@ export type ModalType = 'student' | 'teacher' | 'login' | null;
 export interface User {
   id: number;
   email: string;
-<<<<<<< HEAD
   role: 'student' | 'teacher' | 'manager' | 'admin' | 'Student' | 'Teacher' | 'Manager' | 'Admin';
-=======
   role: 'Student' | 'Teacher' | 'Manager' | 'Admin';
->>>>>>> 9eb61c56c0ee2f61c17f17c3b112086ca969d621
   firstName: string;
   lastName: string;
   phone?: string;
