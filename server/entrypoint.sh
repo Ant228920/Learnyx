9c10ef9284e5fd3c6a1edfa6a07ad23675774714
MarcoGr11 <babunbogdan@gmail.com>
2026-06-04 20:32:25 +0300

    fix: remove entrypoint.sh, use CMD directly in Dockerfile

--- server/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/bin/bash
-set -e
-
-echo "Running migrations..."
-python manage.py migrate --noinput
-
-echo "Starting server..."
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --workers 3
